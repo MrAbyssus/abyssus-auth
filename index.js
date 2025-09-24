5072a3303188857f2d81759f5ee515cbf58b0bb1
MrAbyssus <alexgonzalezdrums@outlook.com>
2025-09-24 01:08:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -165,8 +165,7 @@ app.get('/', async (req, res) => {
         : `<p>No hay eventos registrados</p>`}
     </section>
   `;
-
- res.send(`
+res.send(`
   <main style="font-family:Segoe UI, sans-serif; background:#0a0a0a; color:#ccc; padding:0; margin:0;">
     <header style="padding:50px 30px; text-align:center; background:#111; box-shadow:0 0 20px #00ffff33;">
       <h1 style="color:#00ffff; font-size:36px; margin-bottom:10px;">🔐 Abyssus Dashboard</h1>
@@ -196,8 +195,6 @@ if (!PORT) throw new Error('❌ Variable PORT no definida por Render');
 app.listen(PORT, () => {
   console.log(`🔐 Abyssus Run activo en Render · Puerto ${PORT}`);
 });
-
-
 
 
 
